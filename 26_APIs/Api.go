097e53f76735e367393b5414f0167c2200bbc782
devhimanshuu <devhimanshuu@gmail.com>
2024-02-07 16:43:40 +0530

    26_APIs

---
+++ 26_APIs/Api.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//model for courses - file
+type Course struct {
+	CourseId string `json:"courseid`
+	CourseName string `json:"coursename`
+	CoursePrice int `json:"price"`
+	Author *Author `json:"author"`
+}
+
+//fake DB 
+var courses []Course
+
+//middleware, helper - file
+func(c *Course) IsEmpty() bool{
+	return c.CourseId==""&&c.CourseName==""
+}
+type Author struct{
+	Fullname string `json:"fullname"`
+	Website string `json:"website"`
+}
+
+//controllers 
+
+//serve home route
+
+func serveHome(w http.ResponseWriter,r *http.Request){
+	w.Write([]byte ("<h1>Welcome to Apis in Golang</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter,r *http.Request){
+	fmt.Println("Get All Courses")
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+func getOneCourse(w http.ResponseWriter,r *http.Request){
+	fmt.Println("Get One Course")
+	w.Header().Set("Content-Type" , "application/json")
+
+	//grab id from request
+	params:=mux.Vars(r)
+
+	//loop through courses , find matching id and return responses
+	for _,course:=range courses{
+		if course.CourseId==params["id"]{
+		json.NewEncoder(w).Encode(course)
+		return 
+		}
+	}
+	json.NewEncoder(w).Encode("No course found with given id")
+	return 
+}
+
+
+
+
+
+
+
+
+
+
+func main() {
+	fmt.Println("Build APIs in Golang")
+
+
+}
