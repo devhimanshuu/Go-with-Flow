e60d41fcefd82515d59974c92d5e195cf44c45f4
devhimanshuu <devhimanshuu@gmail.com>
2024-02-15 09:04:51 +0530

    modified

--- 10_Structs/structs.go
+++ 10_Structs/structs.go
@@ -2,19 +2,54 @@ package main
 
 import "fmt"
 
-
-func main(){
-	fmt.Println("Welcome to structs in golang")
-
-	//no inheritance in golang; no super or parent
-	himanshu:=User{"Himanshu","devhimanshuu@gmail.com",true,22}
-	fmt.Println(himanshu)
-	fmt.Printf("Himanshu details are: %+v\n",himanshu)
-	fmt.Printf("Name is : %+v and Email is %v",himanshu.Name,himanshu.Email)
+type Salary struct {
+	Basic, HRA, TA float64
 }
-type User struct{
-	Name string
-	Email string
-	Status bool
-	Age int  
+
+type Employee struct {
+	FirstName, LastName, Email string
+	Age                        int
+	MonthlySalary              []Salary
+}
+
+func (e Employee) EmpInfo() string {
+	fmt.Println(e.FirstName, e.LastName)
+	fmt.Println(e.Age)
+	fmt.Println(e.Email)
+	for _, info := range e.MonthlySalary {
+		fmt.Println("===================")
+		fmt.Println(info.Basic)
+		fmt.Println(info.HRA)
+		fmt.Println(info.TA)
+	}
+	return "----------------------"
+}
+
+func main() {
+
+	e := Employee{
+		FirstName: "Mark",
+		LastName:  "Jones",
+		Email:     "mark@gmail.com",
+		Age:       25,
+		MonthlySalary: []Salary{
+			Salary{
+				Basic: 15000.00,
+				HRA:   5000.00,
+				TA:    2000.00,
+			},
+			Salary{
+				Basic: 16000.00,
+				HRA:   5000.00,
+				TA:    2100.00,
+			},
+			Salary{
+				Basic: 17000.00,
+				HRA:   5000.00,
+				TA:    2200.00,
+			},
+		},
+	}
+
+	fmt.Println(e.EmpInfo())
 }
